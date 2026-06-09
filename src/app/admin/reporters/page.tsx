d2a423f0d19bbde1855f264c56a52325298f1ab7
Sonu Kumar <thedesiandaz.com@gmail.com>
2026-06-09 23:19:31 +0530

    updatwe final

--- src/app/admin/reporters/page.tsx
+++ src/app/admin/reporters/page.tsx
@@ -1,5 +1,3 @@
-'use server';
-
 import React from 'react';
 import ReportersClient from './ReportersClient';
 import { getReportersList } from '@/actions/reporter';
@@ -10,4 +8,5 @@ export default async function ReportersAdminPage() {
   const list = await getReportersList();
   return <ReportersClient initialList={list} />;
 }
+
 
