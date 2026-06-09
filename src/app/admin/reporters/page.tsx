9f32c07ecefda0b89c3ec48cf644d3995c503c08
Sonu Kumar <thedesiandaz.com@gmail.com>
2026-06-09 23:15:39 +0530

    build

--- src/app/admin/reporters/page.tsx
+++ src/app/admin/reporters/page.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 import ReportersClient from './ReportersClient';
 import { getReportersList } from '@/actions/reporter';
 
+export const dynamic = 'force-dynamic';
+
 export default async function ReportersAdminPage() {
   const list = await getReportersList();
   return <ReportersClient initialList={list} />;
 }
+
